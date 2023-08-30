6c73831e1c327157b76713b01c637967430da42e
Anıl Yılmaz <uyelikanil@gmail.com>
2023-08-30 03:37:54 +0300

    Fix theme

--- app/src/main/java/com/anilyilmaz/nativefirebase/core/designsystem/theme/Type.kt
+++ app/src/main/java/com/anilyilmaz/nativefirebase/core/designsystem/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.anilyilmaz.nativefirebase.ui.theme
+package com.anilyilmaz.nativefirebase.core.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
